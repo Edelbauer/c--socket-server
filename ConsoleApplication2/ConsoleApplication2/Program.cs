884e56e73a08094121f507b03831044e3f6c9ad6
edelbauer <niklas.edelbauer@gmail.com>
2016-07-29 12:36:06 +0200

    initial

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fleck;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static PerformanceCounter cpuCounter;
+        static void Main(string[] args)
+        {
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+
+            var allSockets = new List<IWebSocketConnection>();
+
+                var server = new WebSocketServer("ws://0.0.0.0:8181");
+
+                server.Start(socket =>
+                {
+                    socket.OnOpen = () =>
+                    {
+                        allSockets.Add(socket);
+                    };
+                    socket.OnClose = () =>
+                    {
+                        allSockets.Remove(socket);
+                    };
+                    
+                });
+
+           
+                var input = Console.ReadLine();
+                while (input != "exit")
+                {
+                    foreach (var socket in allSockets.ToList())
+                    {
+                    socket.Send(cpuCounter.NextValue().ToString().Replace(',','.'));
+                    Thread.Sleep(50);
+                    }
+                   
+                }
+
+            }
+
+        }
+    }
+
